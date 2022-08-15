c252d5873cca01acbf353c6533fb99293c2e1719
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-08-15 01:48:48 -0300

    test: add unit test example

---
+++ src/app/App.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+describe('App', () => {
+  it('should render Appt correctly', () => {
+    const { container } = render(<App />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should counter value render with default value equal 0', () => {
+    render(<App />)
+
+    const button = screen.getByTestId(/button-increment/i)
+
+    expect(button.textContent).toContain(0)
+  })
+
+  it('should increment counter', async () => {
+    render(<App />)
+
+    const button = screen.getByTestId(/button-increment/i)
+
+    await userEvent.click(button)
+
+    expect(button.textContent).toContain(1)
+  })
+})
