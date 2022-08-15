3004973b346ab92c1c53ad30725b48440db492cf
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-08-15 17:53:55 -0300

    refactor: app

--- src/app/App.spec.tsx
+++ src/app/App.spec.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
-  it('should render Appt correctly', () => {
+  it('should render App correctly', () => {
     const { container } = render(<App />)
 
     expect(container.firstChild).toMatchSnapshot()
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -49,21 +49,6 @@ export const styles = {
     gap: '$4'
   }),
 
-  card: css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '$4',
-    background: '$gray5',
-    padding: '$3',
-    borderRadius: '$3',
-
-    color: '$gray12',
-    cursor: 'pointer',
-
-    fontSize: '$2'
-  }),
-
   footer: css({
     marginTop: 'auto',
     marginBottom: '$4',
